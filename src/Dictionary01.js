746bd7a35a5518679da2d52e719a2bea9e41e031
May-Hlyan-Moe <129745420+May-Hlyan-Moe@users.noreply.github.com>
2023-08-04 18:11:04 +0630

    add SheCodes api

--- src/Dictionary01.js
+++ src/Dictionary01.js
@@ -1,11 +1,21 @@
 import React, { useState } from "react";
 import "./Dictionary01.css";
+import axios from "axios";
 
 export default function Dictionary01() {
     let [keyword, setKeyword] = useState("");
+
+    function handleReponse (response) {
+        console.log(response.data.word);
+    }
+
     function search (event) {
         event.preventDefault();
         alert(`Searching for ${keyword}`);
+
+        let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=e8b0a10cf3eacat9055e9544c4bb244o`;
+        axios.get(apiUrl).then(handleReponse);
+        console.log(apiUrl);
     }
 
     function handleKeywordChange (event) {
